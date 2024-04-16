04851f5dda0c2633bb4713cf302789197c23c959
seichi042I <seichi8901@gmail.com>
2024-04-17 01:31:18 +0900

    micrecorder test

--- frontend/app/src/pages/index.tsx
+++ frontend/app/src/pages/index.tsx
@@ -1,9 +1,42 @@
-import dynamic from "next/dynamic"
+import React, { useRef, MutableRefObject } from "react"
+import { useEffect } from "react"
+import MicRecorder from "@/Components/Molecules/Media/MicRecorder"
 
-const Demo = dynamic(() => import("../Components/Demo/index"), {
-  ssr: false,
-})
+export const Home = () => {
+  const stream:MutableRefObject<MediaStream | undefined> = useRef<MediaStream>()
+  const micRecorder:MutableRefObject<MicRecorder | undefined> = useRef<MicRecorder>()
 
-export default function Home() {
-  return <Demo />
+  useEffect(()=>{
+    const init = async () => {
+      stream.current = await navigator.mediaDevices.getUserMedia({audio:true,video:false})
+      micRecorder.current = new MicRecorder(stream.current)
+      micRecorder.current.onDataAvailable = data => {
+        console.log(data.size)
+      }
+      micRecorder.current.start()
+      console.log("useEffect pass")
+    }
+    init()
+  },[])
+
+  const stop = () => {
+    if(micRecorder.current){
+      micRecorder.current.stop()
+    }
+  }
+
+  const restart = () => {
+    if(micRecorder.current){
+      micRecorder.current.restart()
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={stop}>stop</button>
+      <button onClick={restart}>restart</button>
+    </div>
+  )
 }
+
+export default Home
