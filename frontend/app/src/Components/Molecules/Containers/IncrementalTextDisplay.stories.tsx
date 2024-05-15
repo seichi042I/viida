5bb46ec70b5c49d823d12c2578320a5093d86f62
seichi042I <seichi8901@gmail.com>
2024-05-16 01:48:17 +0900

    名前を変更、文字色のプロパティを追加

--- frontend/app/src/Components/Molecules/Containers/IncrementalTextDisplay.stories.tsx
+++ frontend/app/src/Components/Molecules/Containers/IncrementalTextDisplay.stories.tsx
@@ -1,34 +1,34 @@
-import { StoryObj,Meta } from "@storybook/react";
-import ITD from "./IncrementalTextDisplay";
-import { useRef,useEffect } from "react";
+import { StoryObj, Meta } from "@storybook/react";
+import IncrementalTextDisplay from "./IncrementalTextDisplay";
+import { useRef, useEffect } from "react";
 
 const meta = {
-    component:ITD,
-    title:'Components/Molecules/Containers/IncrementalTextDisplay',
-    parameters:{
-        layout:"centered"
+    component: IncrementalTextDisplay,
+    title: 'Components/Molecules/Containers/IncrementalTextDisplay',
+    parameters: {
+        layout: "centered"
     },
-    args:{
-        animProps:{
-            active:false
+    args: {
+        animProps: {
+            active: false
         }
     }
-} satisfies Meta<typeof ITD>
+} satisfies Meta<typeof IncrementalTextDisplay>
 
 export default meta;
 type Story = StoryObj<typeof meta>
 
 export const Primary = {
-    args:{
-        value:''
+    args: {
+        value: ''
     },
-    render: function Comp({value='this is test.', ...args }:{value:string,}){  // 関数コンポーネントを定義
+    render: function Comp({ value = 'this is test.', ...args }: { value: string, }) {  // 関数コンポーネントを定義
         const contentRef = useRef('')
-        useEffect(()=>{contentRef.current = value},[value])
+        useEffect(() => { contentRef.current = value }, [value])
         return (
-          <meta.component
-            contentRef={contentRef}
-          ></meta.component>
+            <meta.component
+                contentRef={contentRef}
+            ></meta.component>
         );
-      },
+    },
 }
